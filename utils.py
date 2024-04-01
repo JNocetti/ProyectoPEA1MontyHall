f8fedfc51d9f38cf925640d3949c92332e00c2a2
JNocetti <juan.anocetti@gmail.com>
2024-04-01 17:37:31 -0300

    montyHall() aun sin finalizar

--- utils.py
+++ utils.py
@@ -11,26 +11,24 @@ def crearPuertas():
             puertas.append(0)
     return puertas
 
-def chequearSeleccion(seleccion, puertas):
-    if (puertas[seleccion] == 1):
-        return True
-    return False
 
-def demostrarPuerta(eleccion,seleccion,puertas):
-    cont = 0
-    if(eleccion):
-        num = random.randint(0,1)
-        for i in puertas:
-            if (num == 0):
-                if(i == num & i != seleccion):
-                    puertas.remove(i)
-                    break
-            else:
-                if (cont != 0 & i == 0):
-                    puertas.remove(i)
-                    break
-                elif (i == 0):
-                    cont += 1
+def montyHall():
+    puertas  = crearPuertas()
+    seleccion = random.randint(0,2)
+    puertasSinAuto = []
+    for i in range(3):
+        if (i != seleccion & puertas[i] == 0):
+            puertasSinAuto.append(i)
+    puertaAbierta = random.randint(0,1)
+    print("La puerta ",puertasSinAuto[puertaAbierta]," no tiene el premio")
+    print("Desea cambiar de puerta?")
+    eleccion = input()
+    
+        
+
+
+
+
 
             
             
