2e0e538d630d60bbfe813be6af77669e21fe65b6
JNocetti <juan.anocetti@gmail.com>
2024-04-01 22:52:23 -0300

    funciones hechas

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import utils
 import random
 
-utils.MontyHall()
-puertas = utils.crearPuertas()
-## Persona selecciona puerta
-puertaSeleccionada = random.randit(0,2)
-chekSeleccion = utils.chekSeleccion(puertaSeleccionada, puertas)
+def iterarCambiaPuerta(limite):
+    cantidad_ganadas = 0
+    cantidad_perdidas = 0
+    for i in range(limite):
+        if utils.MontyHallCambiaPuerta() == True:
+            cantidad_ganadas += 1
+        else:
+            cantidad_perdidas += 1
+    print("Iteracion de", limite, "veces cambiando de puerta")
+    print("Cantidad de veces que ganó:", cantidad_ganadas)
+    print("Cantidad de veces que perdió:", cantidad_perdidas)
+
+def iterarNoCambiaPuerta(limite):
+    cantidad_ganadas = 0
+    cantidad_perdidas = 0
+    for i in range(limite):
+        if utils.MontyHallNoCambiaPuerta() == True:
+            cantidad_ganadas += 1
+        else:
+            cantidad_perdidas += 1
+    print("Iteracion de", limite, "veces sin cambiar de puerta")
+    print("Cantidad de veces que ganó:", cantidad_ganadas)
+    print("Cantidad de veces que perdió:", cantidad_perdidas)
+
+iterarCambiaPuerta(1000)
+iterarCambiaPuerta(10000)
+iterarCambiaPuerta(100000)
+iterarNoCambiaPuerta(1000)
+iterarNoCambiaPuerta(10000)
+iterarNoCambiaPuerta(100000)
+
+        
+
 
--- utils.py
+++ utils.py
@@ -13,35 +13,88 @@ def crearPuertas():
 
 
 def montyHall():
-    puertas  = crearPuertas()
-    seleccion = random.randint(0,2)
-    puertasSinAuto = []
-    for i in range(3):
-        if (i != seleccion & puertas[i] == 0):
-            puertasSinAuto.append(i)
-    puertaAbierta = random.randint(0,1)
-    print("La puerta ",puertasSinAuto[puertaAbierta]," no tiene el premio")
-    print("Desea cambiar de puerta?")
-    eleccion = input()
+    puertas = crearPuertas()
+    seleccion = random.randint(0, 2)
     
-        
+    print("Su selección es:", seleccion)
+
+    puertas_sin_auto = []
+
+    for i in range(3):
+        if i != seleccion and puertas[i] == 0:
+            puertas_sin_auto.append(i)
+
+    if len(puertas_sin_auto) == 2:
+        # la funcion random.choice() elige un elemento aleatorio de una lista
+        puerta_abierta = random.choice(puertas_sin_auto)
+    else:
+        puerta_abierta = puertas_sin_auto[0]
+
+    print("La puerta", puerta_abierta, "no tiene el premio")
+
+    print("Desea cambiar de puerta? Responda si o no")
+    eleccion = input().lower()
+
+    if "si" in eleccion:
+        for i in range(3):
+            if i != seleccion and i != puerta_abierta:
+                seleccion = i
+                break
+
+    if puertas[seleccion] == 1:
+        print("¡Felicidades, ha ganado!")
+        return True
+    else:
+        print("Lo siento, ha perdido")
+        return False
+
+def MontyHallCambiaPuerta():
+    puertas = crearPuertas()
+    seleccion = random.randint(0, 2)
+    
+    puertas_sin_auto = []
+
+    for i in range(3):
+        if i != seleccion and puertas[i] == 0:
+            puertas_sin_auto.append(i)
+
+    if len(puertas_sin_auto) == 2:
+        # la funcion random.chocie() elige un elemento aleatorio de una lista
+        puerta_abierta = random.choice(puertas_sin_auto)
+    else:
+        puerta_abierta = puertas_sin_auto[0]
+    
+    for i in range(3):
+            if i != seleccion and i != puerta_abierta:
+                seleccion = i
+                break
+
+    if puertas[seleccion] == 1:
+        return True
+    else:
+        return False
+
+    
+def MontyHallNoCambiaPuerta():
+    puertas = crearPuertas()
+    seleccion = random.randint(0, 2)
+    
+    puertas_sin_auto = []
+
+    for i in range(3):
+        if i != seleccion and puertas[i] == 0:
+            puertas_sin_auto.append(i)
+
+    if len(puertas_sin_auto) == 2:
+        # la funcion random.choice() elige un elemento aleatorio de una lista
+        puerta_abierta = random.choice(puertas_sin_auto)
+    else:
+        puerta_abierta = puertas_sin_auto[0]
+
+    if puertas[seleccion] == 1:
+        return True
+    else:
+        return False
 
 
-
-
-
-            
-            
-        
-        
-
-
-
-
-
-
-
-
-
-
-
+   
