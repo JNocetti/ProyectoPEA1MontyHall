d1458532ec536c0963d22029ac91a2d50de59c60
IonasJospo <ionas.josponis@correo.ucu.edu.uy>
2024-04-06 23:07:07 -0300

    Cambios nombres y prints

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
 import utils
 import random
 
-def iterarCambiaPuerta(limite):
+def simularJuegoCambiandoDePuerta(veces):
     cantidad_ganadas = 0
     cantidad_perdidas = 0
-    for i in range(limite):
+    for i in range(veces):
         if utils.MontyHallCambiaPuerta() == True:
             cantidad_ganadas += 1
         else:
             cantidad_perdidas += 1
 
-    print("--------------")
-    print("Iteracion de", limite, "veces cambiando de puerta")
-    print("Cantidad de veces que ganó:", cantidad_ganadas, ", Frecuencia relativa = ",cantidad_ganadas/limite, ", Porcentaje: ",int(cantidad_ganadas/limite*100),"%")
-    print("Cantidad de veces que perdió:", cantidad_perdidas, ", Frecuencia relativa = ",cantidad_perdidas/limite, ", Porcentaje: ",int(cantidad_perdidas/limite*100),"%")
-    print("--------------")
+    
+    print("\nIteracion ", veces)
+    print("Cantidad de veces que ganó:", cantidad_ganadas, ", Frecuencia relativa = ",cantidad_ganadas/veces, ", Porcentaje de ganar: ",int(cantidad_ganadas/veces*100),"%")
+    print("Cantidad de veces que perdió:", cantidad_perdidas, ", Frecuencia relativa = ",cantidad_perdidas/veces, ", Porcentaje de perder: ",int(cantidad_perdidas/veces*100),"%")
+   
 
-def iterarNoCambiaPuerta(limite):
+def simularJuegoSinCambiarDePuerta(veces):
     cantidad_ganadas = 0
     cantidad_perdidas = 0
-    for i in range(limite):
+    for i in range(veces):
         if utils.MontyHallNoCambiaPuerta() == True:
             cantidad_ganadas += 1
         else:
             cantidad_perdidas += 1
     
-    print("--------------")
-    print("Iteracion de", limite, "veces sin cambiar de puerta")
-    print("Cantidad de veces que ganó:", cantidad_ganadas, ", Frecuencia relativa = ",cantidad_ganadas/limite, ", Porcentaje: ",int(cantidad_ganadas/limite*100),"%")
-    print("Cantidad de veces que perdió:", cantidad_perdidas, ", Frecuencia relativa = ",cantidad_perdidas/limite, ", Porcentaje: ",int(cantidad_perdidas/limite*100),"%")
-    print("--------------")
 
-iterarCambiaPuerta(1000)
-iterarCambiaPuerta(10000)
-iterarCambiaPuerta(100000)
-iterarNoCambiaPuerta(1000)
-iterarNoCambiaPuerta(10000)
-iterarNoCambiaPuerta(100000)
+    print("\nIteracion de", veces)
+    print("Cantidad de veces que ganó:", cantidad_ganadas, ", Frecuencia relativa = ",cantidad_ganadas/veces, ", Porcentaje de ganar: ",int(cantidad_ganadas/veces*100),"%")
+    print("Cantidad de veces que perdió:", cantidad_perdidas, ", Frecuencia relativa = ",cantidad_perdidas/veces, ", Porcentaje de perder: ",int(cantidad_perdidas/veces*100),"%")
+   
+
+print("Simulación de Monty Hall cambiando de puerta \n")
+simularJuegoCambiandoDePuerta(1000)
+simularJuegoCambiandoDePuerta(10000)
+simularJuegoCambiandoDePuerta(100000)
+print("-------------------------------------------------------------------------------------------------")
+print("\nSimulación de Monty Hall sin cambiar de puerta \n")
+simularJuegoSinCambiarDePuerta(1000)
+simularJuegoSinCambiarDePuerta(10000)
+simularJuegoSinCambiarDePuerta(100000)
 
         
 
